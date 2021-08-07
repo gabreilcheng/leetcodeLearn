63bfdb52e823c3f6872262dd1c8b4cc68a8da1bc
liyicheng <2217087909@qq.com>
2021-08-07 22:14:19 +0800

    init

---
+++ learn/src/test/java/com/cheng/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheng.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
